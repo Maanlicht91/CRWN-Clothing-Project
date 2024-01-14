35d6dc5ef3ac95367015112367353dce47cb40ee
Maanlicht91 <aynurdemir1991@hotmail.com>
2024-01-14 03:10:53 +0300

    added Link component from React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation</h1>
-      </div>
-      <Outlet/>
-    </div>
-  );
-};
 const Shop = () => {
   return <h1>I am the shop page</h1>;
 };
@@ -18,8 +9,8 @@ const App = () => {
   //! to render homepage with navi we need to use "index"
   return (
     <Routes>
-      <Route path="/" element={<Navigation/>}>
-        <Route index element={<Home />} /> 
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
